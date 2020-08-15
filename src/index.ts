0d3360811fa6012d75930ca3c2b4d57703e15f95
Kik Engineering <engineering@kik.com>
2020-08-14 21:33:40 -0400

    client: better Kin <-> Quark conversions
    GitOrigin-RevId: 20bc50357fd03f39ceee224e444e7ce7e1b33135

--- src/index.ts
+++ src/index.ts
@@ -34,12 +34,23 @@ export enum NetworkPasshrase {
     Test = "Kin Testnet ; December 2018",
 }
 
-export function kinToQuarks(amount: BigNumber | string): BigNumber {
-    return new BigNumber(amount).multipliedBy(1e5);
+// kinToQuarks converts a string representation of kin
+// to the quark value.
+//
+// If the provided kin amount contains more than 5 decimal
+// places (i.e. an inexact number of quarks), additional
+// decimal places will be ignored.
+//
+// For example, passing in a value of "0.000009" will result
+// in a value of 0 quarks being returned.
+//
+export function kinToQuarks(amount: string): BigNumber {
+    const b = new BigNumber(amount).decimalPlaces(5, BigNumber.ROUND_DOWN);
+    return b.multipliedBy(1e5);
 }
 
-export function quarksToKin(amount: BigNumber | string): BigNumber {
-    return new BigNumber(amount).dividedBy(1e5);
+export function quarksToKin(amount: BigNumber | string): string {
+    return new BigNumber(amount).dividedBy(1e5).toString();
 }
 
 export function xdrInt64ToBigNumber(i64: xdr.Int64): BigNumber {
--- test/model.spec.ts
+++ test/model.spec.ts
@@ -1,8 +1,32 @@
 import { xdr } from "stellar-base";
-import { xdrInt64ToBigNumber } from "../src";
+import { xdrInt64ToBigNumber, kinToQuarks, quarksToKin } from "../src";
 import BigNumber from "bignumber.js";
 
 test("XdrInt64ToBigNumber", () => {
     const i64 = new xdr.Int64(1145307136, 572653568);
     expect(xdrInt64ToBigNumber(i64)).toStrictEqual(new BigNumber("2459528347643019264"));
 });
+
+test("kin to quark conversion", () => {
+    const validCases = new Map<string, string>([
+        ["0.00001", "1"],
+		["0.00002", "2"],
+        ["1", "1e5"],
+		["2", "2e5"],
+		// 10 trillion, more than what's in cicrulation
+        ["10000000000000", "1e18"],
+    ])
+    validCases.forEach((expected, input) => {
+        expect(kinToQuarks(input)).toStrictEqual(new BigNumber(expected));
+        expect(quarksToKin(expected)).toStrictEqual(new BigNumber(input).toString());
+    })
+
+    const roundedCases = new Map<string, string>([
+        ["0.000001", "0"],
+        ["0.000015", "1"],
+        ["0.000018", "1"],
+    ]);
+    roundedCases.forEach((expected, input) => {
+        expect(kinToQuarks(input)).toStrictEqual(new BigNumber(expected));
+    })
+})
